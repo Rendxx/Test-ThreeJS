7d08adfa20efc93bcae7d09a58c0276f3d580016
Rendxx <rendxx@gmail.com>
2015-12-07 18:29:40 -0600

    tutorial 2

--- TestThreeJS/test/sample.js
+++ TestThreeJS/test/sample.js
@@ -1,4 +1,29 @@
-﻿$(function () {
-    $('.word')[0].innerHTML += "<br/>JS works well if this line appears.";
-    // get your codes here
-});
+﻿var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 500);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+var axis = new THREE.AxisHelper(10);
+scene.add(axis);
+
+
+var geometry = new THREE.BoxGeometry(5, 5, 5);
+var material = new THREE.MeshBasicMaterial({ color: 0xdddddd, wireframe: true });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.x = 40;
+camera.position.y = 40;
+camera.position.z = 40;
+camera.lookAt(scene.position);
+
+function render() {
+    requestAnimationFrame(render);
+    cube.rotation.x += 0.1;
+    cube.rotation.y += 0.1;
+    renderer.render(scene, camera);
+}
+//render();
+renderer.render(scene, camera);
