0fe087e76f4a7145dc327cad99e063905622ab7a
Rendxx <rendxx@gmail.com>
2016-02-08 15:09:25 -0600

    Setup animation test env

---
+++ TestThreeJS/Animation/sample.js
@@ -0,0 +1,114 @@
+﻿$(function () {
+    var scene, camera, renderer;
+    var controls, guiControls, datGUI;
+    var planeGeometry;
+    var planeMaterial;
+    var axis, grid, color;
+    var stats;
+    var SCREEN_WIDTH, SCREEN_HEIGHT;
+
+    /*variables for lights*/
+
+    function init() {
+        /*creates empty scene object and renderer*/
+        scene = new THREE.Scene();
+        camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, .1, 5000);
+        renderer = new THREE.WebGLRenderer({ antialias: true });
+
+        renderer.setClearColor(0x000000);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.shadowMapEnabled = true;
+        renderer.shadowMapSoft = true;
+
+        /*add controls*/
+        controls = new THREE.OrbitControls(camera, renderer.domElement);
+        controls.addEventListener('change', render);
+
+        /*adds helpers*/
+        axis = new THREE.AxisHelper(10);
+        scene.add(axis);
+
+        grid = new THREE.GridHelper(50, 5);
+        color = new THREE.Color("rgb(255,0,0)");
+        grid.setColors(color, 0x000000);
+        /*scene.add(grid);*/
+
+        /*create plane*/
+        planeGeometry = new THREE.PlaneGeometry(100, 100, 100);
+        planeMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff });
+        var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+        plane.rotation.x = -.5 * Math.PI;
+        plane.receiveShadow = true;
+        scene.add(plane);
+
+        camera.position.x = 40;
+        camera.position.y = 40;
+        camera.position.z = 40;
+        camera.lookAt(scene.position);
+
+        guiControls = {
+        };
+
+        arrayOfLights = [
+            new THREE.AmbientLight(),
+            new THREE.SpotLight()
+        ];
+        
+        var light = new THREE.AmbientLight();
+        // Ambient
+        light.color.setHex(0x333333);
+        scene.add(light);
+
+
+        //datGUI = new dat.GUI();
+
+        $("#webGL-container").append(renderer.domElement);
+        // status track
+        stats = new Stats();
+        stats.domElement.style.position = 'absolute';
+        stats.domElement.style.left = '0px';
+        stats.domElement.style.top = '0px';
+        $("#webGL-container").append(stats.domElement);
+
+        //AddBlenderMesh('table.2.json');
+        //AddBlenderMesh('xyz.json');
+    }
+
+    function AddBlenderMesh(file) {
+        var loader = new THREE.JSONLoader();
+        var mesh = null
+        loader.load(file, function (geometry, materials) {
+            mesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
+            mesh.castShadow = true;
+            mesh.receiveShadow = true;
+            scene.add(mesh);
+        });
+    }
+
+    function render() {
+        /*necessary to make lights function*/
+        //cubeMaterial.needsUpdate = true;
+        //torMaterial.needsUpdate = true;
+        planeMaterial.needsUpdate = true;
+    }
+
+    function animate() {
+        requestAnimationFrame(animate);
+        render();
+        stats.update();
+        renderer.render(scene, camera);
+    }
+
+    $(window).resize(function () {
+        SCREEN_WIDTH = window.innerWidth;
+        SCREEN_HEIGHT = window.innerHeight;
+
+        camera.aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+    });
+
+    init();
+    animate();
+});
