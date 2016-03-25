492eee74a80c8fe604e6ec839b3904811ab27f16
Rendxx <rendxx@gmail.com>
2016-03-25 15:58:41 -0500

    Setup testing env for sprite text

---
+++ TestThreeJS/Sprite-Text/sample.js
@@ -0,0 +1,147 @@
+﻿$(function () {
+    var scene, camera, renderer;
+    var cameraOrtho, sceneOrtho;
+    var planeGeometry;
+    var planeMaterial;
+    var plane;
+    var controls, guiControls;
+    
+    function init() {
+        var SCREEN_WIDTH = window.innerWidth;
+        var SCREEN_HEIGHT = window.innerHeight;
+        /*creates empty scene object and renderer*/
+        camera = new THREE.PerspectiveCamera(45, SCREEN_WIDTH / SCREEN_HEIGHT, .1, 5000);
+        camera.position.z = 1500;
+
+        cameraOrtho = new THREE.OrthographicCamera(-SCREEN_WIDTH / 2, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, -SCREEN_HEIGHT / 2, 1, 10);
+        cameraOrtho.position.z = 10;
+
+        scene = new THREE.Scene();
+        sceneOrtho = new THREE.Scene();
+
+        // render
+        renderer = new THREE.WebGLRenderer();
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.autoClear = false; // To allow render overlay on top of sprited sphere
+        renderer.setSize(window.innerWidth, window.innerHeight);
+
+        /*add controls*/
+        controls = new THREE.OrbitControls(camera, renderer.domElement);
+        controls.addEventListener('change', render);
+
+        addObj();
+        createHUDSprites();
+        
+        $("#webGL-container").append(renderer.domElement);
+    }
+
+    function createHUDSprites() {
+
+        var spritey = makeTextSprite(" Hello, ", { fontsize: 24, borderColor: { r: 255, g: 0, b: 0, a: 1.0 }, backgroundColor: { r: 255, g: 100, b: 100, a: 0.8 } });
+        spritey.position.set(0, 0, 1);
+        sceneOrtho.add(spritey);
+    }
+    
+    function makeTextSprite(message, parameters) {
+        if (parameters === undefined) parameters = {};
+
+        var fontface = parameters.hasOwnProperty("fontface") ?
+            parameters["fontface"] : "Arial";
+
+        var fontsize = parameters.hasOwnProperty("fontsize") ?
+            parameters["fontsize"] : 18;
+
+        var borderThickness = parameters.hasOwnProperty("borderThickness") ?
+            parameters["borderThickness"] : 4;
+
+        var borderColor = parameters.hasOwnProperty("borderColor") ?
+            parameters["borderColor"] : { r: 0, g: 0, b: 0, a: 1.0 };
+
+        var backgroundColor = parameters.hasOwnProperty("backgroundColor") ?
+            parameters["backgroundColor"] : { r: 255, g: 255, b: 255, a: 1.0 };
+
+        var canvas = document.createElement('canvas');
+        var context = canvas.getContext('2d');
+        context.font = "Bold " + fontsize + "px " + fontface;
+
+        // get size data (height depends only on font size)
+        var metrics = context.measureText(message);
+        var textWidth = metrics.width;
+
+        // background color
+        context.fillStyle = "rgba(" + backgroundColor.r + "," + backgroundColor.g + ","
+                                      + backgroundColor.b + "," + backgroundColor.a + ")";
+        // border color
+        context.strokeStyle = "rgba(" + borderColor.r + "," + borderColor.g + ","
+                                      + borderColor.b + "," + borderColor.a + ")";
+
+        context.lineWidth = borderThickness;
+        // 1.4 is extra height factor for text below baseline: g,j,p,q.
+
+        // text color
+        context.fillStyle = "rgba(230, 30, 30, 1.0)";
+
+        context.fillText(message, borderThickness, fontsize + borderThickness);
+
+        // canvas contents will be used for a texture
+        var texture = new THREE.Texture(canvas)
+        texture.needsUpdate = true;
+
+        var spriteMaterial = new THREE.SpriteMaterial({ map: texture });
+        var sprite = new THREE.Sprite(spriteMaterial);
+        sprite.scale.set(100, 50, 1.0);
+        return sprite;
+    }
+    
+    function addObj() {
+        /*create plane*/
+        planeGeometry = new THREE.PlaneGeometry(100, 100, 100);
+        planeMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff });
+        plane = new THREE.Mesh(planeGeometry, planeMaterial);
+
+        /*position and add objects to scene*/
+        plane.receiveShadow = true;
+        scene.add(plane);
+
+        /*scene lights*/
+        ambient = new THREE.AmbientLight();
+        scene.add(ambient);
+    };
+
+    function render() {
+        planeMaterial.needsUpdate = true;
+
+        renderer.clear();
+        renderer.render(scene, camera);
+        renderer.clearDepth();
+        renderer.render(sceneOrtho, cameraOrtho);
+    }
+
+    function animate() {
+        requestAnimationFrame(animate);
+        render();
+        renderer.render(scene, camera);
+    }
+
+    $(window).resize(function () {
+        var SCREEN_WIDTH = window.innerWidth;
+        var SCREEN_HEIGHT = window.innerHeight;
+
+        camera.aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+        camera.updateProjectionMatrix();
+
+
+        cameraOrtho.left = -SCREEN_WIDTH / 2;
+        cameraOrtho.right = SCREEN_WIDTH / 2;
+        cameraOrtho.top = SCREEN_HEIGHT / 2;
+        cameraOrtho.bottom = -SCREEN_HEIGHT / 2;
+        cameraOrtho.updateProjectionMatrix();
+
+        updateHUDSprites();
+
+        renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+    });
+
+    init();
+    animate();
+});
