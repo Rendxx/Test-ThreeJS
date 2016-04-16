72779bbae41129c4ccf31333726ee4420ce4eeb9
Rendxx-PC <rendxx@gmail.com>
2016-04-15 22:57:47 -0500

    test play multiple time

--- TestThreeJS/Howler/sample.js
+++ TestThreeJS/Howler/sample.js
@@ -1,10 +1,13 @@
 ﻿var sound = new Howl({
-    urls: ['step.mp3'],
+    urls: ['test.mp3'],
     loop: true
-}).play();
+});
 
 var t = 0;
-var timer = setInterval(function () {
-    sound.play(); t++;
-    if (t == 30) clearInterval(timer);
-}, 200);
+
+var play = function () {
+    sound.play();
+};
+var stop = function () {
+    sound.stop();
+};
