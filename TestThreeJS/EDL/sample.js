5a5d77577d98f677c9befd6cdfdbadd9b56fdcf5
Rendxx <rendxx@gmail.com>
2016-08-03 16:31:26 -0500

    testing

--- TestThreeJS/EDL/sample.js
+++ TestThreeJS/EDL/sample.js
@@ -4,6 +4,7 @@
     var container = $("#webGL-container")[0];
     var raycaster = new THREE.Raycaster();
     var building = null;
+    var uniforms = [];
 
     /*variables for lights*/
     var ambient;
@@ -13,7 +14,7 @@
         var SCREEN_HEIGHT = window.innerHeight;
         /*creates empty scene object and renderer*/
         camera = new THREE.PerspectiveCamera(45, SCREEN_WIDTH / SCREEN_HEIGHT, .1, 5000);
-        camera.position.z = 200;
+        camera.position.z = 100;
 
         scene = new THREE.Scene();
 
@@ -26,7 +27,12 @@
 
         /*add controls*/
         controls = new THREE.OrbitControls(camera, renderer.domElement);
-        controls.addEventListener('change', render);
+        controls.addEventListener('change', function () {
+
+            var c = [camera.position.x, camera.position.y, camera.position.z];
+            for (var i = 0; i < uniforms.length; i++) uniforms[i]["cameraPos"].value = c;
+            render();
+        });
 
         addObj();
 
@@ -41,19 +47,42 @@
     }
 
     function addObj() {
+
         var loader = new THREE.ObjectLoader();
         loader.load('/EDL/logoModel/' + 'City-2.json', function (obj) {
             building = obj;
             //obj.rotation.x = -Math.PI / 2;
             obj.position.z = 0;
+            
+            obj.traverse(function (child) {
+                if (child instanceof THREE.Mesh) {
+                    var _uniforms = {
+                        "texture": { type: "t", value: child.material.map },
+                        "cameraPos": { type: "v3", value: [camera.position.x, camera.position.y, camera.position.z ] },
+                    };
+
+                    uniforms.push(_uniforms);
+
+                    var material =
+                      new THREE.ShaderMaterial({
+                          uniforms: _uniforms,
+                          vertexShader: document.getElementById('vertex_shader').textContent,
+                          fragmentShader: document.getElementById('fragment_shader').textContent
+                      });
+                    child.material = material;
+                }
+            });
+
             scene.add(obj);
             isloaded = true;
         });
     };
 
     function render() {
+
         renderer.clear();
         renderer.render(scene, camera);
+
     }
 
     function animate() {
