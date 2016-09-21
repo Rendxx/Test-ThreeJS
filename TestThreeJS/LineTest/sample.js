5a0ca04d49650e574aa7b3422de1cafe26eff779
Rendxx <rendxx@gmail.com>
2016-09-21 14:37:36 -0500

    try changing matrix to make line alway face camera

---
+++ TestThreeJS/LineTest/sample.js
@@ -0,0 +1,86 @@
+﻿$(function () {
+    var scene, camera, renderer;
+    var controls, datGUI, stats;
+    var container = $("#webGL-container")[0];
+    var raycaster = new THREE.Raycaster();
+    raycaster.params.Points.threshold = 10;
+    var lines = [];
+    var panel;
+
+    /*variables for lights*/
+    var ambient;
+
+    function init() {
+        var SCREEN_WIDTH = window.innerWidth;
+        var SCREEN_HEIGHT = window.innerHeight;
+        /*creates empty scene object and renderer*/
+        camera = new THREE.PerspectiveCamera(45, SCREEN_WIDTH / SCREEN_HEIGHT, .1, 5000);
+        camera.position.z = 200;
+
+        scene = new THREE.Scene();
+
+        // render
+        renderer = new THREE.WebGLRenderer();
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.autoClear = false; // To allow render overlay on top of sprited sphere
+        renderer.setSize(window.innerWidth, window.innerHeight);
+
+        /*add controls*/
+        controls = new THREE.OrbitControls(camera, renderer.domElement);
+        controls.addEventListener('change', render);
+
+        addObj();
+
+        $("#webGL-container").append(renderer.domElement);
+        /*stats*/
+        stats = new Stats();
+        stats.domElement.style.position = 'absolute';
+        stats.domElement.style.left = '0px';
+        stats.domElement.style.top = '0px';
+        $("#webGL-container").append(stats.domElement);
+    }
+
+    function addObj() {
+        var geometry = new THREE.PlaneGeometry(5, 80, 32);
+        var material = new THREE.ShaderMaterial({
+            vertexShader: document.getElementById('vertex_shader').textContent,
+            fragmentShader: document.getElementById('fragment_shader').textContent
+        });
+        var line = new THREE.Mesh(geometry, material);
+        line.position.z = 20;
+        lines.push(line);
+        scene.add(line);
+
+        // material
+
+        var geometry = new THREE.PlaneGeometry(100, 100, 32);
+        var material = new THREE.MeshBasicMaterial({ color: 0xeeeeee, side: THREE.DoubleSide });
+        panel = new THREE.Mesh(geometry, material);
+        scene.add(panel);
+    };
+
+    function render() {
+        renderer.clear();
+        renderer.render(scene, camera);
+    }
+
+    function animate() {
+        requestAnimationFrame(animate);
+        render();
+        stats.update();
+    }
+
+    $(window).resize(function () {
+        var SCREEN_WIDTH = window.innerWidth;
+        var SCREEN_HEIGHT = window.innerHeight;
+
+        camera.aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+    });
+
+
+    init();
+    animate();
+});
